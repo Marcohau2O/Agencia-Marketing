21b699b71a61a874f32c1bcf7e989cb4365b21ec
Marcohau20 <haupech2@gmail.com>
2024-11-13 12:09:16 -0500

    navbar listo solo conectar con las vistas

---
+++ agencia-marketing/src/components/ComoTrabajamos.jsx
@@ -0,0 +1,8 @@
+// pages/Home.js
+import React from 'react';
+
+function    ComoTrabajamos() {
+  return <h1>Welcome to the Home Page</h1>;
+}
+
+export default ComoTrabajamos;
--- agencia-marketing/src/components/inicio.jsx
+++ agencia-marketing/src/components/inicio.jsx
@@ -1,39 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../img/Logo_transparent.png";
 
-function app() {
+function App() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [isSubmenu2Open, setIsSubmenu2Open] = useState(false);
+
+  // Toggle menu visibility
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
     return (
         <>
-        <nav class="border-blue-300 bg-blue-100 dark:bg-blue-900 dark:border-blue-800">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src={Logo} class="h-16" alt="DigitalGrow Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DigitalGrow</span>
-            </a>
-            <button data-collapse-toggle="navbar-hamburger" type="button" class="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
-            </button>
-            <div class="hidden w-full" id="navbar-hamburger">
-            <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                <li>
-                <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600" aria-current="page">Home</a>
-                </li>
-                <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Services</a>
-                </li>
-                <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white">Pricing</a>
-                </li>
-                <li>
-                <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Contact</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-        </nav>
+            <nav className="border-blue-300 bg-blue-100 dark:bg-blue-900 dark:border-blue-800">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                  <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src={Logo} className="h-16" alt="DigitalGrow Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                      DigitalGrow
+                    </span>
+                  </a>
+                  <button
+                    onClick={toggleMenu}
+                    type="button"
+                    className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-hamburger"
+                    aria-expanded={isMenuOpen ? "true" : "false"}
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    <svg
+                      className="w-5 h-5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 17 14"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M1 1h15M1 7h15M1 13h15"
+                      />
+                    </svg>
+                  </button>
+                  <div
+                    className={`w-full ${isMenuOpen ? "block" : "hidden"}`}
+                    id="navbar-hamburger"
+                  >
+                    <ul className="flex flex-col font-medium mt-4 rounded-lg bg-transparent dark:bg-transparent shadow-lg">
+                      <li className="relative">
+                        <a
+                          href="#"
+                          className="block py-2 px-3 text-white hover:bg-blue-700 rounded dark:hover:text-white"
+                          onClick={() => setIsSubmenuOpen(!isSubmenuOpen)}
+                        >
+                          Que Hacemos
+                        </a>
+                        {isSubmenuOpen && (
+                          <ul className="block py-2 px-3 text-white hover:bg-blue-700 rounded dark:hover:text-white">
+                            <li>
+                              <a
+                                href="#"
+                                className="block py-2 px-3 md:dark:hover:text-black dark:hover:bg-white dark:hover:text-black"
+                                onClick={() => setIsSubmenu2Open(!isSubmenu2Open)}
+                              >
+                                Marketing
+                              </a>
+                              {isSubmenu2Open && (
+                                <ul className="block py-2 px-3">
+                                  <li>
+                                    <a
+                                      href="#"
+                                      className="block py-2 px-3 md:dark:hover:text-black dark:hover:bg-white dark:hover:text-black"
+                                    >
+                                      Diseño Web
+                                    </a>
+                                  </li>
+                                  <li>
+                                    <a
+                                      href="#"
+                                      className="block py-2 px-3 md:dark:hover:text-black dark:hover:bg-white dark:hover:text-black"
+                                    >
+                                      Redes Sociales
+                                    </a>
+                                  </li>
+                                </ul>
+                              )}
+                            </li>
+                          </ul>
+                        )}
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          className="block py-2 px-3 text-white hover:bg-blue-700 rounded dark:hover:text-white"
+                        >
+                          Quiénes Somos
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          className="block py-2 px-3 text-white hover:bg-blue-700 rounded dark:hover:text-white"
+                        >
+                          Como Trabajamos
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
             
             <div className="flex justify-center items-center h-24">
                 <button className="bg-blue-800 text-white text-2xl py-5 px-[40rem] rounded-full">
@@ -172,4 +251,4 @@ function app() {
     )
 }
 
-export default app;
+export default App;
